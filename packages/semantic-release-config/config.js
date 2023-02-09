4576154ef3fcb9bd54c1fcebbba0b24d8df9d849
bubkoo <bubkoo.wy@gmail.com>
2023-02-09 19:13:03 +0800

    feat: export config function

---
+++ packages/semantic-release-config/config.js
@@ -0,0 +1,65 @@
+module.exports = function (options) {
+  return {
+    plugins: [
+      [
+        '@semantic-release/commit-analyzer',
+        {
+          preset: 'angular',
+          releaseRules: [
+            { breaking: true, release: 'major' },
+            { revert: true, release: 'patch' },
+            { type: 'feat', release: 'minor' },
+            { type: 'build', release: 'patch' },
+            { type: 'ci', release: false },
+            { type: 'chore', release: false },
+            { type: 'docs', release: 'patch' },
+            { type: 'perf', release: 'patch' },
+            { type: 'refactor', release: 'patch' },
+            { type: 'style', release: 'patch' },
+            { type: 'test', release: 'patch' },
+            { scope: 'no-release', release: false },
+          ],
+          parserOpts: {
+            noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES'],
+          },
+          ...options.commitAnalyzer,
+        },
+      ],
+      [
+        '@semantic-release/release-notes-generator',
+        {
+          ...options.releaseNotesGenerator,
+        },
+      ],
+      [
+        '@semantic-release/changelog',
+        {
+          ...options.changelog,
+        },
+      ],
+      [
+        '@semantic-release/npm',
+        {
+          ...options.npm,
+        },
+      ],
+      [
+        '@semantic-release/github',
+        {
+          addReleases: 'bottom',
+          ...options.github,
+        },
+      ],
+      [
+        '@semantic-release/git',
+        {
+          assets: ['package.json', 'CHANGELOG.md'],
+          message:
+            // eslint-disable-next-line no-template-curly-in-string
+            'chore(release): ${nextRelease.gitTag} [skip ci]\n\n${nextRelease.notes}',
+          ...options.git,
+        },
+      ],
+    ],
+  }
+}
