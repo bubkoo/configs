3deb1381de6a2b23054f72684c878182eebecbae
bubkoo <bubkoo.wy@gmail.com>
2023-03-06 18:05:37 +0800

    feat: add log message

--- packages/config-util/src/init-config.js
+++ packages/config-util/src/init-config.js
@@ -14,6 +14,7 @@ module.exports = function (filename, content, filenames, pkgJsonKey) {
   for (let i = 0; i < filenames.length; i++) {
     const file = path.join(cwd, filenames[i])
     if (fs.existsSync(file)) {
+      console.log(`Config file "${filenames[i]}" already exists, skipping.`)
       return
     }
   }
@@ -22,10 +23,14 @@ module.exports = function (filename, content, filenames, pkgJsonKey) {
     const pkgJsonFile = path.join(cwd, 'package.json')
     const pkgJson = readJson(pkgJsonFile)
     if (pkgJson[pkgJsonKey]) {
+      console.log(
+        `Config with key "${pkgJsonKey}" already exists in package.json, skipping.`,
+      )
       return
     }
   }
 
   const file = path.join(cwd, filename)
   fs.writeFileSync(file, content)
+  console.log(`Config file "${filename}" created.`)
 }
