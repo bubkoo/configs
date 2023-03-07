a3aad7eca9b2ab0c0e5a60dcbfad48b08f4adc3d
bubkoo <bubkoo.wy@gmail.com>
2023-03-07 11:29:44 +0800

    feat: set peerDependencies optional

--- packages/lint-staged-config/lint-staged.config.js
+++ packages/lint-staged-config/lint-staged.config.js
@@ -1,7 +1,14 @@
+const { hasAnyDeps } = require('@bubkoo/config-util')
+
+const deps = hasAnyDeps(['eslint'])
+const handlers = ['prettier --write']
+if (deps.eslint) {
+  handlers.push(
+    'eslint --cache --cache-location node_modules/.cache/eslint/.eslintcache --fix',
+  )
+}
+
 module.exports = {
   '*': ['prettier --write --ignore-unknown'],
-  '*.{js,jsx,ts,tsx}': [
-    'prettier --write',
-    'eslint --cache --cache-location node_modules/.cache/eslint/.eslintcache --fix',
-  ],
+  '*.{js,jsx,ts,tsx}': handlers,
 }
