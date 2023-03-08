0d5ae9cc640fbe960e14f01e8da7e719daa9e66b
bubkoo <bubkoo.wy@gmail.com>
2023-03-08 17:26:49 +0800

    fix: fix conflicting with `unicorn/better-regex`

--- packages/eslint-config/src/plugins/regexp.js
+++ packages/eslint-config/src/plugins/regexp.js
@@ -1,5 +1,14 @@
 module.exports = {
   plugins: ['regexp'],
   extends: ['plugin:regexp/recommended'],
-  rules: {},
+  rules: {
+    // Conflicting with `unicorn/better-regex`
+    'regexp/sort-character-class-elements': 'off',
+
+    // Conflicting with `unicorn/better-regex`
+    'regexp/match-any': ['error', { allows: ['dotAll', '[\\S\\s]'] }],
+
+    // Conflicting with `unicorn/better-regex`
+    'regexp/strict': 'off',
+  },
 }
