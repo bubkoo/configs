c79223e23dfb70930254d543222d763787bcf3c4
bubkoo <bubkoo.wy@gmail.com>
2023-03-08 10:19:25 +0800

    refactor: remove custom rules

--- packages/commitlint-config/commitlint.config.js
+++ packages/commitlint-config/commitlint.config.js
@@ -1,7 +1,3 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
-  rules: {
-    'body-max-line-length': [2, 'always', 300],
-    'footer-max-line-length': [2, 'always', 300],
-  },
 }
