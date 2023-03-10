8a73a689bc40654d73670cc3866bab6f02ecc1b9
bubkoo <bubkoo.wy@gmail.com>
2023-03-10 16:40:52 +0800

    feat: turn off some rules

--- packages/eslint-config/src/plugins/unicorn.js
+++ packages/eslint-config/src/plugins/unicorn.js
@@ -2,12 +2,18 @@ module.exports = {
   plugins: ['unicorn'],
   extends: ['plugin:unicorn/recommended'],
   rules: {
-    'unicorn/filename-case': ['warn', { case: 'kebabCase' }],
     'unicorn/prefer-module': 'off',
     'unicorn/no-process-exit': 'off',
     'unicorn/no-for-loop': 'off',
     'unicorn/no-null': 'off',
     'unicorn/no-array-for-each': 'off',
+    'unicorn/no-array-reduce': 'off',
+    'unicorn/no-array-callback-reference': 'warn',
+
+    'unicorn/expiring-todo-comments': 'warn',
+    'unicorn/consistent-function-scoping': 'warn',
+    'unicorn/filename-case': ['warn', { case: 'kebabCase' }],
+
     'unicorn/prevent-abbreviations': [
       'error',
       {
