69dc286436a64046182b4ca0728ffc45390d6e2a
bubkoo <bubkoo.wy@gmail.com>
2024-10-12 16:02:51 +0800

    fix: fix setup script

--- packages/eslint-config/setup.js
+++ packages/eslint-config/setup.js
@@ -1,10 +1,5 @@
 import { initConfig } from '@bubkoo/config-util'
 
-const configFileName = 'eslint.config'
 const configContent = `module.exports = require('@bubkoo/eslint-config')`.trim()
 
-initConfig(configFileName, `${configContent}\n`, [
-  `${configFileName}.js`,
-  `${configFileName}.cjs`,
-  `${configFileName}.mjs`,
-])
+initConfig('eslint.config.mjs', `${configContent}\n`)
--- packages/eslint-config/src/common/stylistic.js
+++ packages/eslint-config/src/common/stylistic.js
@@ -12,6 +12,8 @@ export default [
     name: `${name}/${overrideName}`,
     rules: {
       '@stylistic/quote-props': ['error', 'as-needed'],
+      '@stylistic/brace-style': ['error', '1tbs', { allowSingleLine: true }],
+      '@stylistic/operator-linebreak': ['error', 'after'],
     },
   },
 ]
